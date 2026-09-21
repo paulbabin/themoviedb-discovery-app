805084302b129f891cfc8d506c87b82ce35c903c
Paul Babin <babinp207@gmail.com>
2026-09-21 16:19:03 +0000

    style: 🎨 💄 réorganiser la structure HTML et améliorer le style de l'application

--- src/front-end/App.tsx
+++ src/front-end/App.tsx
@@ -14,17 +14,29 @@ export default function App() {
       });
   }, []);
   return (
-    <div>
-      <h1>Popular Movies</h1>
-      {movies ? (
-        <ul>
-          {movies.map((movie) => (
-            <MovieItem key={movie.id} movie={movie} />
-          ))}
-        </ul>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
+    <main>
+      <header>
+        <h1>Films populaires</h1>
+        <h2>
+          Films tendances en France, d'après les données de{' '}
+          <b>The Movie Database</b>
+        </h2>
+      </header>
+      <section>
+        {movies ? (
+          <ul>
+            {movies.map((movie) => (
+              <li key={movie.id}>
+                <article>
+                  <MovieItem movie={movie} />
+                </article>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </section>
+    </main>
   );
 }
